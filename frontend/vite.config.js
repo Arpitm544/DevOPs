a341be74998ef76b8429f9272dcf3ac8b8625911
Arpit Maurya <arpitbabu802@gmail.com>
2025-05-30 05:41:56 +0530

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
@@ -18,6 +19,11 @@ export default defineConfig({
   server: {
     headers: {
       'Content-Type': 'application/javascript'
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
     }
   }
 })
